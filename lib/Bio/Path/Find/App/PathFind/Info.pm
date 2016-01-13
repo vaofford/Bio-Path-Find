7a62990f6de45dccb35356471a603aa0003e63f8
John Tate <jt6@sanger.ac.uk>
2016-01-13 11:04:03 +0000

    Flesh out infofind
    Now has the full functionality of the original. Dumps info to STDOUT in
    the same format as the original, or writes a sensible CSV file. Still
    needs tests.

--- lib/Bio/Path/Find/App/PathFind/Info.pm
+++ lib/Bio/Path/Find/App/PathFind/Info.pm
@@ -13,6 +13,12 @@ use Carp qw ( carp );
 use Path::Class;
 use Try::Tiny;
 
+use Types::Standard qw(
+  ArrayRef
+  Str
+  +Bool
+);
+
 use Bio::Path::Find::Types qw(
   PathClassFile FileFromStr
   BioPathFindDatabaseManager
@@ -43,14 +49,58 @@ lane(s).
 #- public attributes -----------------------------------------------------------
 #-------------------------------------------------------------------------------
 
+# option 'outfile' => (
+#   documentation => 'write output to file',
+#   is            => 'rw',
+#   isa           => PathClassFile->plus_coercions(FileFromStr),
+#   cmd_aliases   => 'o',
+#   cmd_env       => 'PF_OUTFILE',
+#   default       => sub { file 'infofind.out' },
+# );
+
+#---------------------------------------
+
+# this option can be used as a simple switch ("-l") or with an argument
+# ("-l mydir"). It's a bit fiddly to set that up...
+
 option 'outfile' => (
-  documentation => 'write output to file',
-  is            => 'rw',
-  isa           => PathClassFile->plus_coercions(FileFromStr),
+  documentation => 'write info to a CSV file',
+  is            => 'ro',
   cmd_aliases   => 'o',
   cmd_env       => 'PF_OUTFILE',
-  default       => sub { file 'infofind.out' },
+  trigger       => \&_check_for_outfile_value,
+  # no "isa" because we want to accept both Bool and Str and it doesn't seem to
+  # be possible to specify that using the combination of MooseX::App and
+  # Type::Tiny that we're using here
 );
+
+# set up a trigger that checks for the value of the "outfile" command-line
+# argument and tries to decide if it's a boolean, in which case we'll generate
+# a directory name to hold links, or a string, in which case we'll treat that
+# string as a directory name.
+sub _check_for_outfile_value {
+  my ( $self, $new, $old ) = @_;
+
+  if ( not defined $new ) {
+    # write info to file specified by the user
+    $self->_outfile_flag(1);
+  }
+  elsif ( not is_Bool($new) ) {
+    # write info to file specified by the user
+    $self->_outfile_flag(1);
+    $self->_outfile( file $new );
+  }
+  else {
+    # don't write file. Shouldn't ever get here
+    $self->_outfile_flag(0);
+  }
+}
+
+# private attributes to store the (optional) value of the "outfile" attribute.
+# When using all of this we can check for "_outfile_flag" being true or false,
+# and, if it's true, check "_outfile" for a value
+has '_outfile'      => ( is => 'rw', isa => PathClassFile, default => sub { file 'infofind.csv' } );
+has '_outfile_flag' => ( is => 'rw', isa => Bool );
 
 #-------------------------------------------------------------------------------
 #- private attributes ----------------------------------------------------------
@@ -102,9 +152,37 @@ Find information about samples according to the input parameters.
 sub run {
   my $self = shift;
 
+  # if we're writing to file, check that the output file doesn't exist. If we
+  # leave it to _write_csv to check, we could end up searching for lanes for
+  # hours and THEN fail, which would leave the user mildly updset. Better to
+  # fail early, before we've done any work at all.
+  if ( $self->_outfile_flag and -f $self->_outfile ) {
+    Bio::Path::Find::Exception->throw(
+      msg => q(ERROR: CSV file ") . $self->_outfile . q(" already exists; not overwriting existing file)
+    );
+  }
+
+  # find lanes
   my $lanes = $self->_finder->find_lanes(
     ids  => $self->_ids,
     type => $self->type,
+  );
+
+  my $pb = $self->config->{no_progress_bars}
+         ? 0
+         : Term::ProgressBar::Simple->new( {
+             name   => 'collecting info',
+             count  => scalar @$lanes,
+             remove => 1,
+           } );
+
+  # gather the info. We could collect and print the info in the same loop, but
+  # then we wouldn't be able to show the progress bar, which is probably worth
+  # doing. Instead we'll print in a separate loop at the end.
+
+  # start with headers
+  my @info = (
+    [ 'Lane', 'Sample', 'Supplier Name', 'Public Name', 'Strain' ]
   );
 
   foreach my $lane ( @$lanes ) {
@@ -118,11 +196,26 @@ sub run {
     # and get the corresponding row in sequencescape_warehouse.current_sample
     my $row = $self->_ss_db->schema->resultset('CurrentSample')->find( { internal_id => $ssid } );
 
-    print $lane->row->name, "\t";
-    print $lane->row->latest_library->latest_sample->name, "\t";
-    print $row->supplier_name || 'na', "\t";
-    print $row->public_name || 'na', "\t";
-    print $row->strain || 'na', "\n";
+    push @info, [
+      $lane->row->name,
+      $lane->row->latest_library->latest_sample->name,
+      $row->supplier_name || 'NA',
+      $row->public_name || 'NA',
+      $row->strain || 'NA',
+    ];
+
+    $pb++;
+  }
+
+  # write a CSV file or print to STDOUT
+  if ( $self->_outfile_flag ) {
+    $self->_write_csv( \@info, $self->_outfile );
+    say STDERR q(Wrote info to ") . $self->_outfile . q(");
+  }
+  else {
+    # fix the formats of the columns so that everything lines up
+    # (printf format taken from the old pathfind)
+    printf "%-15s %-25s %-25s %-25s %-20s\n", @$_ for @info;
   }
 
 }
