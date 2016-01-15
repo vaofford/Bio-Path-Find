e289893e02cbe46bcd674bfa5a5887c31125ad07
John Tate <jt6@sanger.ac.uk>
2016-01-15 15:30:43 +0000

    Set up travis
    Add a travis config and a script to install dependencies.

---
+++ install-travis-dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cpanm Dist::Zilla
+
+git clone https://github.com:sanger-pathogens/Bio-Track-Schema.git
+git clone https://github.com/sanger-pathogens/Bio-Sequencescape-Schema.git
+
+cd Bio-Track-Schema
+dzil authordeps --missing | cpanm
+dzil listdeps --missing | cpanm
+dzil install
+
+cd ../Bio-Sequencescape-Schema
+dzil authordeps --missing | cpanm
+dzil listdeps --missing | cpanm
+dzil install
+
