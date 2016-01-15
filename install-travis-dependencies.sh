c688da792897f6613dc7ce68890ed2e84400bf2b
John Tate <jt6@sanger.ac.uk>
2016-01-15 15:53:17 +0000

    Fix github URLs for schema modules

--- install-travis-dependencies.sh
+++ install-travis-dependencies.sh
@@ -2,7 +2,7 @@
 
 cpanm Dist::Zilla
 
-git clone https://github.com:sanger-pathogens/Bio-Track-Schema.git
+git clone https://github.com/sanger-pathogens/Bio-Track-Schema.git
 git clone https://github.com/sanger-pathogens/Bio-Sequencescape-Schema.git
 
 cd Bio-Track-Schema
