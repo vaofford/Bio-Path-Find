f33401a6977f1acef20d589acc98942a3f4d57c1
John Tate <jgtate@gmail.com>
2016-02-05 10:33:52 +0000

    Don't assume there's a "config" attribute available
    The _create_pb method tries to check the config to see if "no progress
    bars" is true. That fails if the calling object doesn't apply the
    HasConfig Role and therefore doesn't have a config attribute. This
    change checks if the object "can('config')" before trying to check the
    config.

--- lib/Bio/Path/Find/Role/HasProgressBar.pm
+++ lib/Bio/Path/Find/Role/HasProgressBar.pm
@@ -17,15 +17,16 @@ path-help@sanger.ac.uk
 #- public methods --------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
-# build a progress bar. If "no_progress_bar" is set to true in the config, we
-# don't create a progress bar but return zero, so that the caller can still
-# increment the progress bar without any ill effects. If we do create a
-# progress bar, "remove" is always set to true.
+# build a progress bar. If there is a config method on the object and
+# "no_progress_bar" is set to true in the config, we don't create a progress
+# bar but return zero, so that the caller can still increment the progress bar
+# without any ill effects. If we do create a progress bar, "remove" is always
+# set to true.
 
 sub _create_pb {
   my ( $self, $name, $max ) = @_;
 
-  my $pb = $self->config->{no_progress_bars}
+  my $pb = ( $self->can('config') and $self->config->{no_progress_bars} )
          ? 0
          : Term::ProgressBar::Simple->new( {
              name   => $name,
