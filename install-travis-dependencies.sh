a54c4102f7d03b15f765a7e1fb993585086d348e
John Tate <jgtate@gmail.com>
2016-03-15 11:35:28 +0000

    Update travis install script with new dependencies

--- install-travis-dependencies.sh
+++ install-travis-dependencies.sh
@@ -4,6 +4,8 @@ cpanm Dist::Zilla --notest
 
 git clone https://github.com/sanger-pathogens/Bio-Track-Schema.git
 git clone https://github.com/sanger-pathogens/Bio-Sequencescape-Schema.git
+git clone https://github.com/sanger-pathogens/Bio-AutomatedAnnotation.git
+git clone https://github.com/sanger-pathogens/Bio-Metagenomics.git
 
 cd Bio-Track-Schema
 dzil authordeps --missing | cpanm --notest
@@ -11,6 +13,16 @@ dzil listdeps --missing | cpanm --notest
 dzil install
 
 cd ../Bio-Sequencescape-Schema
+dzil authordeps --missing | cpanm --notest
+dzil listdeps --missing | cpanm --notest
+dzil install
+
+cd ../Bio-AutomatedAnnotation
+dzil authordeps --missing | cpanm --notest
+dzil listdeps --missing | cpanm --notest
+dzil install
+
+cd ../Bio-Metagenomics
 dzil authordeps --missing | cpanm --notest
 dzil listdeps --missing | cpanm --notest
 dzil install
