0c9a5d7becf321076037fb55745c186509888ad2
John Tate <jgtate@gmail.com>
2016-03-21 17:27:28 +0000

    Fix travis install script
    The addition of Bio::Metagenomics has broken the script that installs
    dependencies under travis. Add a bit of code from that package's own
    travis installation script, which will hopefully make it work...

--- install-travis-dependencies.sh
+++ install-travis-dependencies.sh
@@ -2,27 +2,27 @@
 
 cpanm Dist::Zilla --notest
 
-git clone https://github.com/sanger-pathogens/Bio-Track-Schema.git
-git clone https://github.com/sanger-pathogens/Bio-Sequencescape-Schema.git
-git clone https://github.com/sanger-pathogens/Bio-AutomatedAnnotation.git
 git clone https://github.com/sanger-pathogens/Bio-Metagenomics.git
 
-cd Bio-Track-Schema
+cd Bio-Metagenomics
+mkdir -p build/fake_bin && cd build/fake_bin
+touch kraken kraken-build kraken-report merge_metaphlan_tables.py metaphlan_hclust_heatmap.py
+chmod u+x $(ls) && export PATH=$(pwd):$PATH
+cd ../..
 dzil authordeps --missing | cpanm --notest
 dzil listdeps --missing | cpanm --notest
 dzil install
+
+git clone https://github.com/sanger-pathogens/Bio-Track-Schema.git
+
+cd ../Bio-Track-Schema
+dzil authordeps --missing | cpanm --notest
+dzil listdeps --missing | cpanm --notest
+dzil install
+
+git clone https://github.com/sanger-pathogens/Bio-Sequencescape-Schema.git
 
 cd ../Bio-Sequencescape-Schema
-dzil authordeps --missing | cpanm --notest
-dzil listdeps --missing | cpanm --notest
-dzil install
-
-cd ../Bio-AutomatedAnnotation
-dzil authordeps --missing | cpanm --notest
-dzil listdeps --missing | cpanm --notest
-dzil install
-
-cd ../Bio-Metagenomics
 dzil authordeps --missing | cpanm --notest
 dzil listdeps --missing | cpanm --notest
 dzil install
